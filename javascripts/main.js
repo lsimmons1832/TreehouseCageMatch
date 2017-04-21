506ec33e76d88359de1ae7d7242b7902e33e06e4
Latasha Simmons <lsimmons1832@gmail.com>
2017-04-21 11:30:59 -0500

    working on isolating the points array

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,25 +6,51 @@ $(document).ready(function() {
 		let bothPlayers = [];
 		let player1 = [];
 		let player2 = [];
+		let isWinner = "";
+		let totalPointsP1 = [];
+		let totalPointsP2 = [];
 
 		const seperatePlayers = () =>{
 			$.each(bothPlayers, (index, val) => {
-				console.log('ind',index);
-				console.log('val',val);
 				if(index === 0){
-					console.log('I made it past the if');
 					player1.push(val);
 				}else if(index === 1){
 					player2.push(val);
 				}
-					console.log('player1 array', player1);
-					console.log('player2 array', player2);
-				}
 			});
+			//determineWinner();
+			getPoints();
 		};
 
+		// const determineWinner = () =>{
+		// 	const totalPointsP1 = player1["points"];
+	 //  	const totalPointsP2 = player2["points"];
+  // 		if(totalPointsP1 > totalPointsP2){
+  // 			isWinner = player1.name;
+  // 		}else{
+  // 			isWinner = player2.name;
+  // 		}
+		// }
 
-
+  const getPoints = () => { 
+  	player1.forEach((value)=>{
+  		player1.points = [];
+  		totalPointsP1.push(value);
+  	});
+  	console.log(totalPointsP1);
+    // const totalPointsP1 = player1["points"];
+    // const p1Number = totalPointsP1.indexOf(points.total);
+    // const totalPointsP2 = player2["points"];
+    // const p2Number = totalPointsP2.indexOf(points.total);  
+    // if (p1Number > p2Number){ 
+    //   isWinner = player1.name;
+    // } else {
+    //   isWinner = player2.name;
+    // }
+  };
+// console.log("show me player1", player1);
+// console.log("show me player2", player2);
+// console.log("who wins?", isWinner);
 
 
 
@@ -58,28 +84,12 @@ $(document).ready(function() {
 Promise.all([loadPlayer1(), loadPlayer2()])
 	.then((players) =>{
 			bothPlayers = players;
-			console.log("this is the data being pushed", players);
-			console.log("this is the master array players", bothPlayers);
 			seperatePlayers();
 			}).catch((errors) => {
 				alert(errors);
 			console.log(errors);
 		});
 	});
-
-//   loadPlayer1().then((player1) => {
-//     player1.forEach((player) => {
-//       player1.push(player);
-//     });
-//     getInput();
-// });
-
-//     loadPlayer2().then((player2) => {
-//     player2.forEach((player) => {
-//       player2.push(player);
-//     });
-
-// });
 
 
 
